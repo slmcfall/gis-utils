f6d81ec5fe76a76268581da490a72e743c18b7ee
Sean <sean.l.mcfall@gmail.com>
2015-04-14 11:02:45 -0400

    cleanGeometry updated with st prefix additions

---
+++ cleanGeometry.sql
@@ -0,0 +1,75 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Name: cleangeometry(geometry); Type: FUNCTION; Schema: public; Owner: sean
+--
+
+CREATE FUNCTION cleangeometry(geometry) RETURNS geometry
+    LANGUAGE plpgsql
+    AS $_$DECLARE
+  inGeom ALIAS for $1;
+  outGeom geometry;
+  tmpLinestring geometry;
+
+Begin
+  
+  outGeom := NULL;
+  
+-- Clean Process for Polygon 
+  IF (GeometryType(inGeom) = 'POLYGON' OR GeometryType(inGeom) = 'MULTIPOLYGON') THEN
+
+-- Only process if geometry is not valid, 
+-- otherwise put out without change
+    if not st_isValid(inGeom) THEN
+    
+-- create nodes at all self-intersecting lines by union the polygon boundaries
+-- with the startingpoint of the boundary.  
+      tmpLinestring := st_union(st_multi(st_boundary(inGeom)),st_pointn(st_boundary(inGeom),1));
+      outGeom = st_buildarea(tmpLinestring);      
+      IF (GeometryType(inGeom) = 'MULTIPOLYGON') THEN      
+        RETURN st_multi(outGeom);
+      ELSE
+        RETURN outGeom;
+      END IF;
+    else    
+      RETURN inGeom;
+    END IF;
+
+
+------------------------------------------------------------------------------
+-- Clean Process for LINESTRINGS, self-intersecting parts of linestrings 
+-- will be divided into multiparts of the mentioned linestring 
+------------------------------------------------------------------------------
+  ELSIF (GeometryType(inGeom) = 'LINESTRING') THEN
+    
+-- create nodes at all self-intersecting lines by union the linestrings
+-- with the startingpoint of the linestring.  
+    outGeom := st_union(st_multi(inGeom),st_pointn(inGeom,1));
+    RETURN outGeom;
+  ELSIF (GeometryType(inGeom) = 'MULTILINESTRING') THEN 
+    outGeom := multi(st_union(st_multi(inGeom),st_pointn(inGeom,1)));
+    RETURN outGeom;
+  ELSE 
+    RAISE NOTICE 'The input type % is not supported',GeometryType(inGeom);
+    RETURN inGeom;
+  END IF;	  
+End;$_$;
+
+
+ALTER FUNCTION public.cleangeometry(geometry) OWNER TO sean;
+
+--
+-- PostgreSQL database dump complete
+--
+
